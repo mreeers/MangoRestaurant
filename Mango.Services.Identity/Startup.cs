bc1bd77d73eeaabdeee818a1f1425df126f640d8
Александр Ефанов <verpl01@yandex.ru>
2021-06-19 00:49:45 +0500

    Configure and run Identity Server

--- Mango.Services.Identity/Startup.cs
+++ Mango.Services.Identity/Startup.cs
@@ -1,6 +1,10 @@
+using Mango.Services.Identity.DbContext;
+using Mango.Services.Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +28,24 @@ namespace Mango.Services.Identity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(opt =>
+            {
+                opt.Events.RaiseErrorEvents = true;
+                opt.Events.RaiseInformationEvents = true;
+                opt.Events.RaiseFailureEvents = true;
+                opt.Events.RaiseSuccessEvents = true;
+                opt.EmitStaticAudienceClaim = true;
+            }).AddInMemoryIdentityResources(SD.IdentityResources)
+            .AddInMemoryApiScopes(SD.ApiScopes)
+            .AddInMemoryClients(SD.Clients)
+            .AddAspNetIdentity<ApplicationUser>();
+
+            builder.AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +65,8 @@ namespace Mango.Services.Identity
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
